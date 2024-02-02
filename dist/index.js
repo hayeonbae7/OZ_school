88e7f1f875d43bee9b41e44779271d97b9f8fa41
hayeonbae7 <baehayeon@naver.com>
2024-02-02 21:57:35 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// console.log("hello typescript");
+Object.defineProperty(exports, "__esModule", { value: true });
+// const nums: number[] = [];
+// const a = 10 + nums[0];
+// const someString = "typescript";
+// someString.endsWith("t") //ES2022정도부터 사용 가능 문법
+const util_1 = require("./util");
+(0, util_1.printName)("typescript");
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// console.log("hello typescript");
+
+// const nums: number[] = [];
+
+// const a = 10 + nums[0];
+
+// const someString = "typescript";
+// someString.endsWith("t") //ES2022정도부터 사용 가능 문법
+import { printName } from "./util";
+printName("typescript")
